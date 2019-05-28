cf1e481f3af4980452c119f4e9dc55670ef09a8b
LCTO-TLCO <y.s.y.s.s1013@gmail.com>
2019-05-28 09:31:55 +0900

    * modyfy

--- analyze/analyze.py
+++ analyze/analyze.py
@@ -11,12 +11,16 @@ import datetime
 
 
 def read_data():
-    data_file = "../RaspSkinnerBox/log/test_action.csv"
-    script_file = "./reshape_data.R"
+    data_file = "../RaspSkinnerBox/log/no011_action.csv"
     time_limit_delta = 60
     header = ["timestamps", "task", "session_id", "correct_times", "event_type", "hole_no"]
     data = pd.read_csv(data_file, names=header, parse_dates=[0], dtype={'hole_no': 'str'})
-    data = data[data["event_type"].isin(["reward", "failure", "time over"])]
+
+    # TODO omission抜き集計とomission有り集計の2種を切り替えて出す
+#    data = data[data["event_type"].isin(["reward", "failure", "time over"])]
+    data = data[data["event_type"].isin(["reward", "failure"])]
+
+
     data = data.reset_index()
     # task interval
     task_start_index = [0]
@@ -94,42 +98,58 @@ def read_data():
     #         continue
     #     data["burst_group"][i] = data["burst_group"][i - 1] + 1
 
+    # TODO task毎にaction prob集計
+
     # action Probability
-    after_c_all = float(len(data[data["is_correct"] & data.shift()["is_omission"]]))
+    after_c_all = float(len(data[data["is_correct"] == 1]))
     after_f_all = float(len(data[data["is_incorrect"] == 1]))
-    after_c_starts = data[data["is_correct"] & data.shift()["is_omission"]]
-    after_f_starts = data[(data["is_incorrect"] == 1) & (data.shift()["is_omission"]==1)]
+    after_c_starts = data[data["is_correct"] == 1]
+    after_f_starts = data[data["is_incorrect"] == 1]
 
     # after_o_all = len(data[data["event_type"] == "time over"])
-    prob_index = ["c_same", "f_diff", "c_omit", "f_omit", "c_NotMax", "f_NotMax", "o_NotMax"]
-    probability = pd.DataFrame(columns=prob_index, index=range(1, 5)).fillna(0.0)
+    forward_trace = 10
+    prob_index = ["c_same", "c_diff", "c_omit", "c_checksum", "f_same", "f_diff", "f_omit", "f_checksum", "c_NotMax", "f_NotMax", "o_NotMax"]
+    probability = pd.DataFrame(columns=prob_index, index=range(1, forward_trace)).fillna(0.0)
     # count
+    # correctスタート
     for idx, dt in after_c_starts.iterrows():
         is_continued = True
-        for j in range(1, min(5, len(data) - idx)):
-            # 連続で報酬を得ているときの処理
+        for j in range(1, min(forward_trace, len(data) - idx)):
+            # 報酬を得たときと同じ選択(CF両方)をしたときの処理
             if dt["hole_no"] == data.shift(-j)["hole_no"][idx] and is_continued:
                 probability["c_same"][j] = probability["c_same"][j] + 1
+            # omissionの場合
             elif data.shift(-j)["is_omission"][idx]:
                 probability["c_omit"][j] = probability["c_omit"][j] + 1
-                is_continued = False
-            else:
-                is_continued = False
+                # is_continued = False
+            elif dt["hole_no"] != data.shift(-j)["hole_no"][idx] and is_continued:
+                probability["c_diff"][j] = probability["c_diff"][j] + 1
+            # 違うhole
+#            else:
+#                is_continued = False
+    # incorrectスタート
     for idx, dt in after_f_starts.iterrows():
         is_continued = True
-        for j in range(1, min(5, len(data) - idx)):
-            if data.shift(-j)["is_omission"][idx] and is_continued:
+        for j in range(1, min(forward_trace, len(data) - idx)):
+            # 連続で失敗しているときの処理
+            if dt["hole_no"] == data.shift(-j)["hole_no"][idx] and is_continued:
+                probability["f_same"][j] = probability["f_same"][j] + 1
+            elif data.shift(-j)["is_omission"][idx] and is_continued:
                 probability["f_omit"][j] = probability["f_omit"][j] + 1
-            elif not dt["hole_no"] == data.shift(-j)["hole_no"][idx]:
+            elif dt["hole_no"] != data.shift(-j)["hole_no"][idx] and not data.shift(-j)["is_omission"][idx] and is_continued:
                 probability["f_diff"][j] = probability["f_diff"][j] + 1
-                is_continued = False
+                # is_continued = False
             else:
                 is_continued = False
     # calculate
     probability["c_same"] = probability["c_same"] / after_c_all if not after_c_all == 0 else 0.0
-    probability["f_diff"] = probability["f_diff"] / after_f_all if not after_f_all == 0 else 0.0
+    probability["c_diff"] = probability["c_diff"] / after_c_all if not after_c_all == 0 else 0.0
     probability["c_omit"] = probability["c_omit"] / after_c_all if not after_c_all == 0 else 0.0
+    probability["c_checksum"] = probability["c_same"] + probability["c_diff"] + probability["c_omit"]
+    probability["f_same"] = probability["f_same"] / after_f_all if not after_f_all == 0 else 0.0
+    probability["f_diff"] = probability["f_diff"] / after_f_all if not after_f_all == 0 else 0.0
     probability["f_omit"] = probability["f_omit"] / after_f_all if not after_f_all == 0 else 0.0
+    probability["f_checksum"] = probability["f_same"] + probability["f_diff"] + probability["f_omit"]
 
     # prob$c_NotMax %/=% after_c_all
     # prob$f_NotMax %/=% after_f_all
@@ -174,3 +194,4 @@ if __name__ == "__main__":
     data.to_csv('./test.csv')
     prob.to_csv('./prob.csv')
     # graph(data)
+
