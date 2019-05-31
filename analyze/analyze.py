c8726babd60221f00a9af47a80071d74772bbb01
LCTO-TLCO <y.s.y.s.s1013@gmail.com>
2019-05-31 15:25:13 +0900

    * add figures drowing function

--- analyze/analyze.py
+++ analyze/analyze.py
@@ -46,7 +46,7 @@ class task_data:
                     end_col_no = len(id_col)
                 else:
                     end_col_no = tmp.index[0]
-                id_col[line_no:end_col_no+1]["session_id"] = session_id
+                id_col[line_no:end_col_no + 1]["session_id"] = session_id
                 line_no = end_col_no + 1
                 session_id = session_id + 1
             data["session_id"] = id_col["session_id"]
@@ -106,24 +106,33 @@ class task_data:
                 data.loc[~data['hole_no'].str.contains(str(hole_no)), "is_hole{}".format(hole_no)] = None
 
             # cumsum
-            for i in range(0, len(task_start_index)):
-                index_start = task_start_index[i]
-                index_end = len(data)
-                if i < len(task_start_index) - 1:
-                    index_end = task_start_index[i + 1]
-                pre_correct = data["cumsum_correct"][index_start] if not i == 0 else 0
-                pre_incorrect = data["cumsum_incorrect"][index_start] if not i == 0 else 0
-                pre_omission = data["cumsum_omission"][index_start] if not i == 0 else 0
-                # warning mettyaderu zone
-                data["cumsum_correct_taskreset"][index_start:index_end] = data["cumsum_correct"][
-                                                                          index_start:index_end] - \
-                                                                          pre_correct
-                data["cumsum_incorrect_taskreset"][index_start:index_end] = data["cumsum_incorrect"][
-                                                                            index_start:index_end] - \
-                                                                            pre_incorrect
-                data["cumsum_omission_taskreset"][index_start:index_end] = data["cumsum_omission"][
-                                                                           index_start:index_end] - \
-                                                                           pre_omission
+            # for i in range(0, len(task_start_index)):
+            #     index_start = task_start_index[i]
+            #     index_end = len(data)
+            #     if i < len(task_start_index) - 1:
+            #         index_end = task_start_index[i + 1]
+            #     pre_correct = data["cumsum_correct"][index_start] if not i == 0 else 0
+            #     pre_incorrect = data["cumsum_incorrect"][index_start] if not i == 0 else 0
+            #     pre_omission = data["cumsum_omission"][index_start] if not i == 0 else 0
+            #     # warning mettyaderu zone
+            #     data["cumsum_correct_taskreset"][index_start:index_end] = data["cumsum_correct"][
+            #                                                               index_start:index_end] - \
+            #                                                               pre_correct
+            #     data["cumsum_incorrect_taskreset"][index_start:index_end] = data["cumsum_incorrect"][
+            #                                                                 index_start:index_end] - \
+            #                                                                 pre_incorrect
+            #     data["cumsum_omission_taskreset"][index_start:index_end] = data["cumsum_omission"][
+            #                                                                index_start:index_end] - \
+            #                                                                pre_omission
+            def add_cumsum():
+                data["cumsum_correct_taskreset"] = data["is_correct"].fillna(0)
+                data["cumsum_incorrect_taskreset"] = data["is_incorrect"].fillna(0)
+                data["cumsum_omission_taskreset"] = data["is_omission"].fillna(0)
+                data["cumsum_correct_taskreset"] = data.groupby("task")["cumsum_correct_taskreset"].cumsum()
+                data["cumsum_incorrect_taskreset"] = data.groupby("task")["cumsum_incorrect_taskreset"].cumsum()
+                data["cumsum_omission_taskreset"] = data.groupby("task")["cumsum_omission_taskreset"].cumsum()
+
+            add_cumsum()
 
             def min_max(x, axis=None):
                 np.array(x)
@@ -241,8 +250,8 @@ class task_data:
                     elif self.data.shift(-j)["is_omission"][idx] and is_continued:
                         probability["f_omit"][j] = probability["f_omit"][j] + 1
                     elif dt["hole_no"] != self.data.shift(-j)["hole_no"][idx] and not \
-                    self.data.shift(-j)["is_omission"][
-                        idx] and is_continued:
+                            self.data.shift(-j)["is_omission"][
+                                idx] and is_continued:
                         probability["f_diff"][j] = probability["f_diff"][j] + 1
                         # is_continued = False
                     else:
@@ -289,8 +298,8 @@ class task_data:
                         elif self.data.shift(-j)["is_omission"][idx] and is_continued:
                             prob["f_omit"][j] = prob["f_omit"][j] + 1
                         elif dt["hole_no"] != self.data.shift(-j)["hole_no"][idx] and not \
-                        self.data.shift(-j)["is_omission"][
-                            idx] and is_continued:
+                                self.data.shift(-j)["is_omission"][
+                                    idx] and is_continued:
                             prob["f_diff"][j] = prob["f_diff"][j] + 1
                             # is_continued = False
                         else:
@@ -325,6 +334,7 @@ class task_data:
 # TODO Entropy, Nose Poke Raster, Correct/Incorrect/Omission(cumsum) を縦に３つ並べる
 # TODO 散布図,csv出力 連続無報酬期間 vs reaction time (タスクコールからnose pokeまでの時間 正誤両方)
 # TODO 散布図,csv出力 連続無報酬期間 vs reward latency  (正解nose pokeからmagazine nose pokeまでの時間 正解のみ)
+
 # TODO 散布図,csv出力 連続無報酬期間 vs 区間Entropy (検討中)
 # TODO 探索行動の短期指標を定義(Exploration Index 1, EI1) : 検討中
 
@@ -340,43 +350,45 @@ class graph:
 
     # data plot
     # TODO これは全nose pokeなので、burstは別に用意する
-    def nose_poke_raster(self):
+    def nose_poke_raster(self, mouse_id, ax):
+        labels = ["correct", "incorrect", "omission"]
+        # flags = data.loc[:, data.colums.str.match("is_[(omission|correct|incorrect)")]
+        datasets = [(self.data.mice_task[mouse_id][self.data.mice_task[mouse_id]
+                                                   ["is_{}".format(flag)] == 1]) for flag in labels]
+        # TODO 同一session_idに複数のhole choiceとomissionが入っているのを修正 session_idが信用できない -> A.rehash する関数実装
+        for dt, la in zip(datasets, labels):
+            ax.scatter(dt['session_id'], dt['is_hole1'] * 1, s=15, color="blue")
+            ax.scatter(dt['session_id'], dt['is_hole3'] * 2, s=15, color="blue")
+            ax.scatter(dt['session_id'], dt['is_hole5'] * 3, s=15, color="blue")
+            ax.scatter(dt['session_id'], dt['is_hole7'] * 4, s=15, color="blue")
+            ax.scatter(dt['session_id'], dt['is_hole9'] * 5, s=15, color="blue")
+            ax.scatter(dt['session_id'], dt['is_omission'] * 0, s=15, color="red")
+        # ax.set_xlabel("time/sessions")
+        # ax.set_ylabel("hole dots")
+        # ax.ylim(-1, 6)
+        # plt.show()
+
+    def CFO_cumsum_plot(self, mouse_id, ax):
+        ax.plot(self.data.mice_task[mouse_id]['cumsum_correct_taskreset'])
+        ax.plot(self.data.mice_task[mouse_id]['cumsum_incorrect_taskreset'])
+        ax.plot(self.data.mice_task[mouse_id]['cumsum_omission_taskreset'])
+        # plt.title('{:03} CFO cumsum'.format(mouse_id))
+        # plt.show()
+
+    def entropy_scatter(self, mouse_id, ax):
+        ax.plot(self.data.mice_task[mouse_id]['hole_choice_entropy'])
+        # ax.ylabel('Entropy (bit)')
+        # plt.title('{:03} Entropy'.format(mouse_id))
+
+    def ent_raster_cumsum(self):
+        fig = plt.figure(figsize=(15, 8), dpi=100)
         for mouse_id in self.mice:
-            burst_nosepoke = plt.figure()
-            burst_ax = burst_nosepoke.add_subplot(1, 1, 1)
-            labels = ["correct", "incorrect", "omission"]
-            # flags = data.loc[:, data.colums.str.match("is_[(omission|correct|incorrect)")]
-            datasets = [(self.data.mice_task[mouse_id][self.data.mice_task[mouse_id]
-                                                       ["is_{}".format(flag)] == 1]) for flag in labels]
-            # TODO 同一session_idに複数のhole choiceとomissionが入っているのを修正 session_idが信用できない -> A.rehash する関数実装
-            for dt, la in zip(datasets, labels):
-                burst_ax.scatter(dt['session_id'], dt['is_hole1'] * 1, s=15, color="blue")
-                burst_ax.scatter(dt['session_id'], dt['is_hole3'] * 2, s=15, color="blue")
-                burst_ax.scatter(dt['session_id'], dt['is_hole5'] * 3, s=15, color="blue")
-                burst_ax.scatter(dt['session_id'], dt['is_hole7'] * 4, s=15, color="blue")
-                burst_ax.scatter(dt['session_id'], dt['is_hole9'] * 5, s=15, color="blue")
-                burst_ax.scatter(dt['session_id'], dt['is_omission'] * 0, s=15, color="red")
-            burst_ax.set_xlabel("time/sessions")
-            burst_ax.set_ylabel("hole dots")
-            plt.ylim(-1, 6)
+            self.entropy_scatter(mouse_id, fig.add_subplot(3, 1, 1))
+            self.nose_poke_raster(mouse_id, fig.add_subplot(3, 1, 2))
+            self.CFO_cumsum_plot(mouse_id, fig.add_subplot(3, 1, 3))
+            plt.title('{:03} graph'.format(mouse_id))
             plt.show()
-
-    def CFO_cumsum_plot(self):
-        for mouse_id in self.mice:
-            fig = plt.figure(figsize=(15, 8), dpi=100)
-            plt.plot(self.data.task_prob[mouse_id]['cumsum_correct_taskreset'])
-            plt.title('{:03} CFO'.format(mouse_id))
-            # .plot.scatter(x='session_id', y='is_correct')
-
-    def entropy_scatter(self):
-        for mouse_id in self.mice:
-            fig = plt.figure(figsize=(15, 8), dpi=100)
-            plt.plot(self.data.mice_task[mouse_id]['hole_choice_entropy'])
-            plt.xlabel('Trial')
-            plt.ylabel('Entropy (bit)')
-            plt.title('{:03} Entropy'.format(mouse_id))
-
-
+            plt.savefig('{}no{:03d}_graph.png'.format(self.exportpath, mouse_id))
 
     def same_plot(self):
         fig = plt.figure(figsize=(15, 8), dpi=100)
@@ -400,7 +412,6 @@ class graph:
 
             plt.savefig('{}no{:03d}_prob.png'.format(self.exportpath, mouse_id))
 
-
     def burst_raster(self):
         None
 
@@ -409,7 +420,6 @@ class graph:
 
     def reward_latency_scatter(self):
         None
-
 
 
 if __name__ == "__main__":
@@ -421,5 +431,6 @@ if __name__ == "__main__":
     task = task_data(mice, tasks, logpath)
     graph_ins = graph(task, mice, tasks, logpath)
     # graph_ins.entropy_scatter()
-    graph_ins.nose_poke_raster()
-    graph_ins.same_plot()
+    # graph_ins.nose_poke_raster()
+    # graph_ins.same_plot()
+    graph_ins.ent_raster_cumsum()
